a4351e656ef16f0425f0622d96f553df0dd15235
muzi55 <muzi5@kakao.com>
2023-10-24 23:37:01 +0900

    리드미 추가

--- README.md
+++ README.md
@@ -1,25 +1,44 @@
-# myvue
+# 나의 첫번째 뷰입니다.
 
-## Project setup
-```
-yarn install
-```
+1. 카운터 만들기
+   `vue`는 `React` 와 다르게 변동되는 값을
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
+```jsx
+data(){
+    return{
+        // 여기에 값을 저장합니다.
+        count : 0,
+    }
+}
 ```
 
-### Lints and fixes files
-```
-yarn lint
+data 함수 안에 key:value 형식으로 저장합니다.
+<br />
+<br />
+<br />
+함수같은 경우에 `methods`라는 객체를 만들어 그곳에 함수를 저장합니다.
+
+```jsx
+methods:{
+    increase(){
+        this.count++;
+    },
+}
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-# myFirstVue
+여기서 this를 써주지 않으면 에러가 나옵니다.
+<br />
+<br />
+<br />
+
+값을 사용할때는 `{{ count }}` 이런식으로 사용합니다.
+
+2. 배열을 반복할시
+   `v-for` 을 사용합니다.
+
+```jsx
+<div v-for="a in food" :key="a">{{ a }}</div>
+  </div>
+```
+
+이런식으로 `for in` 문 처럼 사용해주면 된다.
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>뷰 학습</h1>
+  <div>
+    <h2>카운터 만들기</h2>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>{{ count }}</p>
+    <button v-on:click="increase">증가</button>
+    <button @click="decrease">감소</button>
+  </div>
+  <hr />
+
+  <div>
+    <h2>배열 반복하기/ 반복문</h2>
+
+    <!-- <div>카레</div>
+    <div>마파두부</div>
+    <div>돈까스</div> -->
+
+    <!-- <div>{{ food[0] }}</div>
+    <div>{{ food[1] }}</div>
+    <div>{{ food[2] }}</div> -->
+
+    <!-- <div v-for="(a, i) in food" :key="i">{{ a }}</div> -->
+    <!-- 뷰에서 반복학습 -->
+    <div v-for="a in food" :key="a">{{ a }}</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      count: 0,
+      food: ["카레", "마파두부", "돈까스"],
+    };
+  },
+  methods: {
+    increase() {
+      this.count++;
+    },
+    decrease() {
+      this.count--;
+    },
+  },
+};
 </script>
 
 <style>
